2327c4fa4505344054e8f87c0046172556e8182c
Gerall09 <103030918+Gerall09@users.noreply.github.com>
2023-08-11 22:25:37 -0700

    Add files via upload

---
+++ js/script-type.js
@@ -0,0 +1,13 @@
+/*=== Typed Reveal ===*/
+
+
+
+const typed = new Typed('.multiple-text', {
+strings: ['Multimedia','Multimedia2'],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 1000,
+loop: true
+});
+
+/*=== end ===*/
